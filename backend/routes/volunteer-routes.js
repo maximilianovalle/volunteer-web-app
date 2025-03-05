f2b6ae4fe32a26f57d8337117dd357175e33d47f
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-03-05 11:48:09 -0600

    Add volunteer routes

--- backend/routes/volunteer-routes.js
+++ backend/routes/volunteer-routes.js
@@ -2,11 +2,42 @@
 
 // !- Routes map HTTP methods (like GET, POST, PUT, DELETE) and URL paths to handler functions, which execute when a matching request is received
 
+// CREATE means to add new data.
+// POST is the HTTP method used to create new data.
+// PUT is used to update existing data.
+// DELETE removes existing data.
+
+
+
 const express = Require('express');
 const router = express.Router();
 
+
+
 // Navigate to Pages
 
+const { openEventPage, openNotificationPage, openVolunteerDashboard, openVolunteerHistory } = Require('../controllers/volunteer-controllers');
+
+router.get('/event/:id', openEventPage);
+router.get('/notifications', openNotificationPage);
+router.get('/dashboard', openVolunteerDashboard);
+router.get('/history', openVolunteerHistory);
+
+
+
+// Apply for an Event by ID
+
+const { applyToEvent } = Require('../controllers/volunteer-controllers');
+
+router.put('/event/:id', applyToEvent);
+
+
+
+// Create Notification (Admins)
+
+const { createNotification } = Require('../constrollers/volunteer-controllers');
+
+router.create('/notifications', createNotification);
 
 
 
