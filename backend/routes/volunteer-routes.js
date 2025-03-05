7cda26405aad0709be21d4c40e43cd72aa1370f8
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-03-05 11:35:46 -0600

    Add ExpressJS background framework illustration

--- backend/routes/volunteer-routes.js
+++ backend/routes/volunteer-routes.js
@@ -1,3 +1,15 @@
 // Route: define how an application responds to client requests for specific endpoints.
 
 // !- Routes map HTTP methods (like GET, POST, PUT, DELETE) and URL paths to handler functions, which execute when a matching request is received
+
+const express = Require('express');
+const router = express.Router();
+
+// Navigate to Pages
+
+
+
+
+// Exporting
+
+module.exports = router;
