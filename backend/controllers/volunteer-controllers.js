99a09c74cc738885a13c242fdb93711622f1641a
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-03-05 11:03:55 -0600

    Initialize volunteer route and controller files

---
+++ backend/controllers/volunteer-controllers.js
@@ -0,0 +1,3 @@
+//  Controller: responsible for handling incoming requests and sending responses back to the client - act as intermediaries between routes and application logic. They receive requests, process them using models or services, and then return the appropriate response.
+
+// ! - Controllers get the requested data from the models, create an HTML page displaying the data, and return it to the user.
---
+++ backend/routes/volunteer-routes.js
@@ -0,0 +1,3 @@
+// Route: define how an application responds to client requests for specific endpoints.
+
+// !- Routes map HTTP methods (like GET, POST, PUT, DELETE) and URL paths to handler functions, which execute when a matching request is received
