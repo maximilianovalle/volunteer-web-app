bea49d25e4d1ba815230949d17e2659fa2851a1c
Ethan <ethanma04@gmail.com>
2025-03-20 15:02:41 -0500

    Database JS File
    Adding db.js with connection info and database access and adjusting userprofile frontend options to match database dummy data.

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const {createPool} = require("mysql");
+
+const pool = mysql.createPool({
+    host: "cosc4353-volunteer-group18.mysql.database.azure.com",
+    user: "iAmAnAdmin",
+    password: "oadnAOPP77*",
+    database: "volunteer_db",
+    waitForConnections: true,
+    connectionLimit: 10,
+});
+
