2d4e4e15cda76c7f635cb5c7f3a91aaccc02cabb
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-04-23 19:02:53 -0500

    Modify volunteer dashboard and add browse events page

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
+// serves frontend
+// handles JSON API routes
+
 const express = require("express");
 const path = require('path');
 const app = express();
 const cors = require("cors");
 
-
-
 app.use(cors());
 app.use(express.json()); 
 app.use(express.static(path.join(__dirname, "../frontend")));
+
+// backend
 
 const apiEndpoints = require("./api/loginRoutes.js");
 app.use("/api", apiEndpoints);
@@ -21,7 +24,7 @@ app.use("/api/profile", profileRoutes);
 const volunteerRoutes = require("./routes/volunteer-routes.js");
 app.use("/api/volunteer", volunteerRoutes);
 
-
+// frontend
 
 app.get("/admin-form", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/admin_form/admin_form.html"));
@@ -31,21 +34,15 @@ app.get("/admin-dashboard", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/admin_dashboard/admin_dashboard.html"));
 });
 
-app.get("/volunteer-dashboard", (req, res) => {
+app.get("/volunteer-dashboard", (req, res) => { // !
+    console.log("\nAccessing volunteer dashboard page...\n");
     res.sendFile(path.join(__dirname, "../frontend/volunteer/volunteer-dashboard.html"));
 });
 
-// app.get("/volunteer-history", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend/volunteer/volunteer-history.html"));
-// });
-
-// app.get("/event-page", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend/volunteer/event-page.html"));
-// });
-
-// app.get("/notification-page", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend/volunteer/notification-page.html"));
-// });
+app.get("/browse-events", (req, res) => { // !
+    console.log("\nAccessing browse events page...\n");
+    res.sendFile(path.join(__dirname, '../frontend/volunteer/browse-events.html'));
+});
 
 app.get("/user-manage-profile", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/usermanageprofile/usermanageprofile.html"));
@@ -60,10 +57,9 @@ app.get("/signup", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/Login/sign-up.html"));
 });
 
-
 app.use((req, res) => {
     res.status(404);
-    res.send('<h1>Error 404; Resource not found</h1>')
+    res.send('<h1>Error 404: Resource not found</h1>')
 })
 
 app.listen(process.env.PORT || 3000, () => console.log("App available on http://localhost:3000"));
